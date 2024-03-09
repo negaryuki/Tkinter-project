fc5d41524d183ce7bf1a2e4e746bef8a3fa2f70c
negaryuki <negar.norouzi94@gmail.com>
2024-03-09 21:04:01 +0330

    update

--- Youtube lesson ( Clear Code Projects)/01- Widgets/04-Buttons.py
+++ Youtube lesson ( Clear Code Projects)/01- Widgets/04-Buttons.py
@@ -20,7 +20,7 @@ button.pack()
 
 # checkbutton
 
-check_var = tk.IntVar()   # we could use IntVar  or BooleanVar only the type of the result will change
+check_var = tk.IntVar()  # we could use IntVar  or BooleanVar only the type of the result will change
 
 check_button = ttk.Checkbutton(
     root,
@@ -28,7 +28,7 @@ check_button = ttk.Checkbutton(
     command=lambda: print(check_var.get()),
     variable=check_var,
     onvalue=10,
-    offvalue=5)      # checkbuttons use "variables" instead of "text variables" since they don't return a txt
+    offvalue=5)  # checkbuttons use "variables" instead of "text variables" since they don't return a txt
 check_button.pack()
 
 # Radio Button
@@ -37,19 +37,38 @@ radio_var = tk.StringVar()
 
 radio1 = ttk.Radiobutton(root,
                          text="radio button",
-                         value= 'radio 1',
-                         variable= radio_var,
-                         command= lambda: print(radio_var.get()))
-radio1.pack()              # It is important to set a value for each radio button otherwise all radio buttons will be
-                            # connected and act toghether
+                         value='radio 1',
+                         variable=radio_var,
+                         command=lambda: print(radio_var.get()))
+radio1.pack()  # It is important to set a value for each radio button otherwise all radio buttons will be
+# connected and act together
 
 radio2 = ttk.Radiobutton(root,
                          text="radio button",
-                         value = 2,
-                         variable= radio_var,
-                         command= lambda:print(radio_var.get()))
+                         value=2,
+                         variable=radio_var,
+                         command=lambda: print(radio_var.get()))
 radio2.pack()
 
+
+# -----------------------------------------------
+
+def radio_func():
+    print(check_bool.get())
+    check_bool.set(False)
+
+
+radio_string = tk.StringVar()
+check_bool = tk.BooleanVar()
+
+exercise_radio1 = ttk.Radiobutton(root, text="Radio A", value='A', command=radio_func, variable=radio_string)
+exercise_radio2 = ttk.Radiobutton(root, text="Radio B", value='B', command=radio_func,variable=radio_var)
+
+exercise_check = ttk.Checkbutton(root, text="exercise check box", command=radio_func, variable= radio_string)
+
+exercise_radio1.pack()
+exercise_radio2.pack()
+exercise_check.pack()
 
 # run
 root.mainloop()
