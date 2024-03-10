fe4a7b6534398fcd7a69c34ef46e1c11d21dbcc6
negaryuki <negar.norouzi94@gmail.com>
2024-03-10 12:10:34 +0330

    update

--- Youtube lesson ( Clear Code Projects)/01- Widgets/07- combobox - Spinbox.py
+++ Youtube lesson ( Clear Code Projects)/01- Widgets/07- combobox - Spinbox.py
@@ -24,8 +24,10 @@ combo_label = ttk.Label(root, text='A Label is here ')
 combo_label.pack()
 
 # spinbox
-
-spin=ttk.Spinbox(root,from_= 3, to= 21, increment=3, command=lambda: print('arrow was pressed'))
+spin_int= tk.IntVar(value=12)
+spin = ttk.Spinbox(root, from_=3, to=21, increment=3, command=lambda: print(spin_int.get()))
+spin.bind('<<Increment>>', lambda event: print('up'))
+spin.bind('<<Decrement>>', lambda event: print('down'))
 spin.pack()
 
 # exercise----------------------------------------------------
@@ -33,14 +35,11 @@ spin.pack()
 # and print the value whenever the value is decreased
 
 exercise_letters = ('A', 'B', 'C', 'D', 'E')
-exercise_string = tk.StringVar(value = exercise_letters[0])
-exercise_spin = ttk.Spinbox(window, textvariable = exercise_string, values = exercise_letters)
+exercise_string = tk.StringVar(value=exercise_letters[0])
+exercise_spin = ttk.Spinbox(root, textvariable=exercise_string, values=exercise_letters)
 exercise_spin.pack()
 
 exercise_spin.bind('<<Decrement>>', lambda event: print(exercise_string.get()))
-
-# run
-window.mainloop()
 
 # run
 root.mainloop()
