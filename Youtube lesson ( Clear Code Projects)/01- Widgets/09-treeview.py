573c9e4fcbb0c82cde841826ad4dd22bf9427432
negaryuki <negar.norouzi94@gmail.com>
2024-03-19 18:18:41 +0330

    update

--- Youtube lesson ( Clear Code Projects)/01- Widgets/09-treeview.py
+++ Youtube lesson ( Clear Code Projects)/01- Widgets/09-treeview.py
@@ -5,14 +5,20 @@ from tkinter import ttk
 from random import choice
 
 # window
-window = tk.Tk()
-window.geometry('600x400')
-window.title('Treeview')
+root = tk.Tk()
+root.geometry('600x400')
+root.title('Treeview')
+
+# treeview
+table = ttk.Treeview(root, columns=('first', 'last', 'email'), show= 'headings')
+table.heading('first', text='First name')
+table.heading('last', text='Surname')
+table.heading('email', text='Email')
+table.pack()
 
 # data
 first_names = ['Bob', 'Maria', 'Alex', 'James', 'Susan', 'Henry', 'Lisa', 'Anna', 'Lisa']
 last_names = ['Smith', 'Brown', 'Wilson', 'Thomson', 'Cook', 'Taylor', 'Walker', 'Clark']
 
-
 # run
-window.mainloop()
+root.mainloop()
