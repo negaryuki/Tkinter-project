b16e5dacc3b80bbb871e1d7f20bfe1db7328540f
negaryuki <negar.norouzi94@gmail.com>
2024-03-19 18:04:48 +0330

    update

---
+++ Youtube lesson ( Clear Code Projects)/01- Widgets/09-treeview.py
@@ -0,0 +1,18 @@
+# Treeview creates tables
+
+import tkinter as tk
+from tkinter import ttk
+from random import choice
+
+# window
+window = tk.Tk()
+window.geometry('600x400')
+window.title('Treeview')
+
+# data
+first_names = ['Bob', 'Maria', 'Alex', 'James', 'Susan', 'Henry', 'Lisa', 'Anna', 'Lisa']
+last_names = ['Smith', 'Brown', 'Wilson', 'Thomson', 'Cook', 'Taylor', 'Walker', 'Clark']
+
+
+# run
+window.mainloop()
