9d102caab8c63d8d6703bf3a73b46e66774dd05b
negaryuki <negar.norouzi94@gmail.com>
2024-03-19 18:27:52 +0330

    update

--- Youtube lesson ( Clear Code Projects)/01- Widgets/09-treeview.py
+++ Youtube lesson ( Clear Code Projects)/01- Widgets/09-treeview.py
@@ -9,6 +9,10 @@ root = tk.Tk()
 root.geometry('600x400')
 root.title('Treeview')
 
+# data
+first_names = ['Bob', 'Maria', 'Alex', 'James', 'Susan', 'Henry', 'Lisa', 'Anna', 'Lisa']
+last_names = ['Smith', 'Brown', 'Wilson', 'Thomson', 'Cook', 'Taylor', 'Walker', 'Clark']
+
 # treeview
 table = ttk.Treeview(root, columns=('first', 'last', 'email'), show= 'headings')
 table.heading('first', text='First name')
@@ -16,9 +20,16 @@ table.heading('last', text='Surname')
 table.heading('email', text='Email')
 table.pack()
 
-# data
-first_names = ['Bob', 'Maria', 'Alex', 'James', 'Susan', 'Henry', 'Lisa', 'Anna', 'Lisa']
-last_names = ['Smith', 'Brown', 'Wilson', 'Thomson', 'Cook', 'Taylor', 'Walker', 'Clark']
+# insert values
+
+#table.insert(parent='', index= 0,values=('Jane','Doe','JaneDoe2024@email.com'))
+
+for i in range(100):
+    first = choice(first_names)
+    last = choice(last_names)
+    email = f'{first[0]}{last}@email.com'
+    data = (first, last, email)
+    table.insert(parent='', index=0, values=data)
 
 # run
 root.mainloop()
