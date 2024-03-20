ae6694823cad475f64aebf30358a04da8d5b69cf
negaryuki <negar.norouzi94@gmail.com>
2024-03-21 01:00:21 +0330

    update

--- Youtube lesson ( Clear Code Projects)/01- Widgets/09-treeview.py
+++ Youtube lesson ( Clear Code Projects)/01- Widgets/09-treeview.py
@@ -32,5 +32,30 @@ for i in range(100):
     table.insert(parent='', index=0, values=data)
 
 table.insert(parent='', index=tk.END, values=('XXXXX', 'YYYYY', 'ZZZZZ'))
+
+# table.insert(parent = '', index = 0, values = ('John', 'Doe', 'JohnDoe@email.com'))
+for i in range(100):
+	first = choice(first_names)
+	last = choice(last_names)
+	email = f'{first[0]}{last}@email.com'
+	data = (first, last, email)
+	table.insert(parent = '', index = 0, values = data)
+
+table.insert(parent = '', index = tk.END, values = ('XXXXX', 'YYYYY', 'ZZZZZ'))
+
+# events
+def item_select(_):
+	print(table.selection())
+	for i in table.selection():
+		print(table.item(i)['values'])
+	# table.item(table.selection())
+
+def delete_items(_):
+	print('delete')
+	for i in table.selection():
+		table.delete(i)
+
+table.bind('<<TreeviewSelect>>', item_select)
+table.bind('<Delete>', delete_items)
 # run
 root.mainloop()
