c395b84196db1cf1aabea690dddbed2135e52987
negaryuki <negar.norouzi94@gmail.com>
2024-03-21 03:18:45 +0330

    update

--- Youtube lesson ( Clear Code Projects)/01- Widgets/10-Sliders.py
+++ Youtube lesson ( Clear Code Projects)/01- Widgets/10-Sliders.py
@@ -4,6 +4,7 @@
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import scrolledtext
 
 # Window
 
@@ -13,13 +14,44 @@ root.title("Sliders")
 # slider
 scale_float = tk.DoubleVar(value=20)  # tk.IntVar only stores int values
 scale = ttk.Scale(root,
-                  command=lambda value: print(value),
+                  command=lambda value: progress.stop(),
                   from_=0,
                   to=25,
                   length=300,
                   orient='vertical',
                   variable=scale_float)
 scale.pack()
+
+# Progress Bar
+progress = ttk.Progressbar(root,
+                           variable=scale_float,
+                           maximum=25,
+                           orient="horizontal",
+                           mode='determinate',  # or "indeterminate"
+                           length=300)
+
+# progress.start(1000)  # not much useful
+progress.pack()
+
+# Scrolledtext
+
+scroll = scrolledtext.ScrolledText(root,
+                                   width=20,
+                                   height=5)
+scroll.pack()
+
+# ------------------------------
+
+exercise_int = tk.IntVar()
+exercise_progress = ttk.Progressbar(root, orient='vertical', variable=exercise_int)
+exercise_progress.pack()
+exercise_progress.start()
+
+label = ttk.Label(root, textvariable=exercise_int)
+label.pack()
+
+exercise_scale = ttk.Scale(root, variable=exercise_int, from_=0, to=100)
+exercise_scale.pack()
 
 # run
 root.mainloop()
