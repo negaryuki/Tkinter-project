cf9002e05fb488dc1e94c171f5776e55e8709a28
negaryuki <negar.norouzi94@gmail.com>
2024-03-29 21:25:55 +0330

    update

--- Youtube lesson ( Clear Code Projects)/02- Layouts/03-Menu.py
+++ Youtube lesson ( Clear Code Projects)/02- Layouts/03-Menu.py
@@ -9,10 +9,20 @@ root.title('Menu')
 # Menu
 menu = tk.Menu(root)
 
-# Sub menu
+# Sub menu 1
 file_menu = tk.Menu(menu, tearoff=False)
 file_menu.add_command(label="new", command=lambda: print("New file"))
+file_menu.add_command(label="open", command=lambda: print("Open"))
+file_menu.add_separator()
 menu.add_cascade(label="File", menu=file_menu)
+
+# Sub Menu 2
+help_menu = tk.Menu(menu, tearoff=False)
+menu.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="Help Me", command=lambda: print("Help"))
+
+help_check_string = tk.StringVar()
+help_menu.add_checkbutton(label='check',onvalue='on', offvalue= 'off', variable=help_check_string)
 
 root.configure(menu=menu)
 # Run
