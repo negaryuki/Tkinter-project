ee0209562effe5e364d099a99e33a6d17eafd8dd
negaryuki <negar.norouzi94@gmail.com>
2024-03-29 23:56:55 +0330

    update

--- Youtube lesson ( Clear Code Projects)/02- Layouts/01-Intro.py
+++ Youtube lesson ( Clear Code Projects)/02- Layouts/01-Intro.py
@@ -12,8 +12,8 @@ label1 = tk.Label(root, text='Label 1',
 label2 = tk.Label(root, text='Label 2', background='blue')
 
 # pack
-#label1.pack(side='left', expand=True, fill='x')
-#label2.pack(side='left', expand=True, fill='both')
+# label1.pack(side='left', expand=True, fill='x')
+# label2.pack(side='left', expand=True, fill='both')
 
 # Grid
 root.columnconfigure(0, weight=1)
@@ -22,7 +22,12 @@ root.columnconfigure(2, weight=2)
 root.rowconfigure(0, weight=1)
 root.rowconfigure(1, weight=1)
 
-label1.grid(row=0,column=1, sticky= 'nsew')  # to which direction should the widget stick
-label2.grid(row=1,column=1,sticky='nsew', columnspan= 2)
+# label1.grid(row=0,column=1, sticky= 'nsew')  # to which direction should the widget stick
+# label2.grid(row=1,column=1,sticky='nsew', columnspan= 2)
+
+# place
+label1.place(x=100,y=150,width=200,height=100)  # unexpectedly, the higher y number is, the further down the axis goes
+label2.place(relx=0.5, rely=0.4, relwidth=1, anchor='center')   # anchor is the point the widget is located at
+
 # run
 root.mainloop()
