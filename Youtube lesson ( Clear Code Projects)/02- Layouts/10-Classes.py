c9f14ae52309dfb9b1af8a5b0b4c2ef2efc64af9
negaryuki <negar.norouzi94@gmail.com>
2024-04-05 20:36:20 +0330

    update

--- Youtube lesson ( Clear Code Projects)/02- Layouts/10-Classes.py
+++ Youtube lesson ( Clear Code Projects)/02- Layouts/10-Classes.py
@@ -4,28 +4,29 @@ from tkinter import ttk
 
 class App(tk.Tk):
     def __init__(self, title, size):
-        # Main Setup
-        super().__init__()  # ensures that tk works well
+        # main setup
+        super().__init__()    # ensures that tk works well
         self.title(title)
         self.geometry(f'{size[0]}x{size[1]}')
         self.minsize(size[0], size[1])
 
-        # Widgets
+        # widgets
         self.menu = Menu(self)
+        self.main = Main(self)
 
-        # Run
+        # run
         self.mainloop()
 
 
 class Menu(ttk.Frame):
     def __init__(self, parent):
-        super().__init__(parent)  # calling the object which is the parent
+        super().__init__(parent)    # calling the object which is the parent
         self.place(x=0, y=0, relwidth=0.3, relheight=1)  # Placing the frame
+
         self.create_widgets()
 
     def create_widgets(self):
-
-        # Create Widgets
+        # create the widgets
         menu_button1 = ttk.Button(self, text='Button 1')
         menu_button2 = ttk.Button(self, text='Button 2')
         menu_button3 = ttk.Button(self, text='Button 3')
@@ -39,14 +40,14 @@ class Menu(ttk.Frame):
 
         entry = ttk.Entry(self)
 
-        # Create Grid
+        # create the grid
         self.columnconfigure((0, 1, 2), weight=1, uniform='a')
         self.rowconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
 
-        # Place widgets on Grid
-        menu_button1.grid(row=0, column=0, sticky='nswe', columnspan=2, padx=4, pady=4)
-        menu_button2.grid(row=0, column=2, sticky='nswe', padx=4, pady=4)
-        menu_button3.grid(row=1, column=0, columnspan=3, sticky='nsew', padx=4, pady=4)
+        # place the widgets
+        menu_button1.grid(row=0, column=0, sticky='nswe', columnspan=2)
+        menu_button2.grid(row=0, column=2, sticky='nswe')
+        menu_button3.grid(row=1, column=0, columnspan=3, sticky='nsew')
 
         menu_slider1.grid(row=2, column=0, rowspan=2, sticky='nsew', pady=20)
         menu_slider2.grid(row=2, column=2, rowspan=2, sticky='nsew', pady=20)
@@ -61,20 +62,25 @@ class Menu(ttk.Frame):
 
 
 class Main(ttk.Frame):
-    def __init__(self,parent):
+    def __init__(self, parent):
         super().__init__(parent)
         self.place(relx=0.3, y=0, relwidth=0.7, relheight=1)
-
-        # create widgets
-
-        frame = ttk.Frame(self)
-        label = ttk.Label(frame, text='text', background='red')
-        button = ttk.Button(frame, text='text')
-
-        label.pack(expand= True, fill= 'both')
-        button.pack(expand=True, fill='both', pady= 10)
-
-        frame.pack(side='left', expand=True, fill='both', padx=20, pady=20)
+        Entry(self, 'Entry 1', 'Button 1', 'green')
+        Entry(self, 'Entry 2', 'Button 2', 'blue')
+        Entry(self, 'Entry 3', 'Button 3', 'green')
 
 
-App("Hi", (600, 600))
+class Entry(ttk.Frame):
+    def __init__(self, parent, label_text, button_text, label_background):
+        super().__init__(parent)
+
+        label = ttk.Label(self, text=label_text, background=label_background)
+        button = ttk.Button(self, text=button_text)
+
+        label.pack(expand=True, fill='both')
+        button.pack(expand=True, fill='both', pady=10)
+
+        self.pack(side='left', expand=True, fill='both', padx=20, pady=20)
+
+
+App('Class based app', (600, 600))
